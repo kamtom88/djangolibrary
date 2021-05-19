898d26842ca819fbb3813e25b7cab2a1fc792bfb
Kamil <kam.tomkiewicz@gmail.com>
2021-05-19 19:26:15 +0200

    backup

--- shelf/urls.py
+++ shelf/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
-from .views import authordetailview, authorlistview, booklistview, publisherlistview, index, contact_view
+from .views import authordetailview, authorlistview, booklistview, publisherlistview, index,\
+    contact_view, bookdetailview
 
 urlpatterns = [
     path('authors/', authorlistview.AuthorListView.as_view(), name='author-list'),
     path('authors/<pk>', authordetailview.AuthorDetailView.as_view(), name='author-detail'),
-    path('books/', booklistview.BookListView.as_view(), name='author-list'),
+    path('books/', booklistview.BookListView.as_view(), name='book-list'),
+    path('books/<pk>', bookdetailview.BookDetailView.as_view(), name='book-detail'),
     path('publishers/', publisherlistview.PublisherListView.as_view()),
     path('',  index.index, name='index'),
     path('contact/',  contact_view.MessageAddView.as_view(), name='contact'),
--- shelf/views/authordetailview.py
+++ shelf/views/authordetailview.py
@@ -5,4 +5,3 @@ from ..models import author
 
 class AuthorDetailView(DetailView):
     model = author.Author
-#
